bdf0548b86a3f94735dfed76290678a777022820
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2024-01-22 06:26:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,14 @@ provider "google" {
 }
 
 
-
 variable "table_schema" {
-  default = <<EOF
-  [
+  type = list(object({
+    name = string
+    type = string
+    mode = string
+    description = string
+  }))
+  default =   [
     {
       "name": "permalink",
       "type": "STRING",
@@ -32,7 +36,40 @@ variable "table_schema" {
       "description": "State where the head office is located"
     }
   ]
-  EOF
+}
+
+locals {
+  schema_json = jsonencode(var.table_schema)
+}
+
+resource "google_bigquery_dataset" "testDataset" {
+  dataset_id                  = var.dataset_id
+  friendly_name               = var.dataset_name
+  description                 = "This is a test description"
+  location                    = var.region
+  default_table_expiration_ms = 3600000
+  delete_contents_on_destroy = true
+
+  labels = {
+    env = "default"
+  }
+}
+
+resource "google_bigquery_table" "testTableRemoveIt" {
+  dataset_id = google_bigquery_dataset.testDataset.dataset_id
+  table_id   = var.table_id
+  deletion_protection=false
+
+  time_partitioning {
+    type = "DAY"
+  }
+
+  labels = {
+    env = "default"
+  }
+
+  schema =  local.schema_json
+
 }
 
 variable "dataset_id" {
@@ -54,38 +91,8 @@ variable "region" {
 	default = "us-central1"
 }
 
-
 variable "zone" {
   default = "us-central1-a"
 }
 
 variable "credentials" {}
-
-resource "google_bigquery_dataset" "testDataset" {
-  dataset_id                  = var.dataset_id
-  friendly_name               = var.dataset_name
-  description                 = "This is a test description"
-  location                    = var.region
-  default_table_expiration_ms = 3600000
-
-  labels = {
-    env = "default"
-  }
-}
-
-resource "google_bigquery_table" "testTableRemoveIt" {
-  dataset_id = google_bigquery_dataset.testDataset.dataset_id
-  table_id   = var.table_id
-  deletion_protection=false
-
-  time_partitioning {
-    type = "DAY"
-  }
-
-  labels = {
-    env = "default"
-  }
-
-  schema = var.table_schema
-
-}
